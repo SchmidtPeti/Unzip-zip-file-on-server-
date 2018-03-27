f76ffb15d107c4a2d9a222b89165c48253e515c1
Whichhunter404 <schmidt.peti32@gmail.com>
2018-03-27 22:26:03 +0200

    I can detect that dir or file

--- openzip.php
+++ openzip.php
@@ -1,10 +1,11 @@
 <?php
 $zip_name ="";
-if(isset($_GET["submit"])){
+if(isset($_GET["Extract"])){
 	$zip_name = $_GET["zip_file_name"];
+  echo $zip_name;
 }
 //without (wordpress)basename do nothing
-if(!isset($zip_name)){
+if(isset($zip_name)){
 //unpack to a dir called sth for example(wordpress,drupal)
 $zip = new ZipArchive;
 if ($zip->open($_SERVER['DOCUMENT_ROOT'].'/'.$zip_name) === TRUE) {
@@ -26,6 +27,11 @@ $source = $_SERVER['DOCUMENT_ROOT']."/wordpress/";
 $destination = $_SERVER['DOCUMENT_ROOT']."/";
 foreach ($files as $file) {
   if (in_array($file, array(".",".."))) continue;
+  if(is_dir($source.$file)){
+      echo 'malac';
+      recurse_copy($source.$file,$destination.$file);
+  }
+  else{
   if (copy($source.$file, $destination.$file)) {
     $delete[] = $source.$file;
   }
@@ -33,7 +39,22 @@ foreach ($files as $file) {
 	foreach ($delete as $file) {
  		 unlink($file);
 	}
+  }
 }
+function recurse_copy($src,$dst) { 
+    $dir = opendir($src); 
+    while(false !== ( $file = readdir($dir)) ) { 
+        if (( $file != '.' ) && ( $file != '..' )) { 
+            if ( is_dir($src . '/' . $file) ) { 
+                recurse_copy($src . '/' . $file,$dst . '/' . $file); 
+            } 
+            else { 
+                copy($src . '/' . $file,$dst . '/' . $file); 
+            } 
+        } 
+    } 
+    closedir($dir); 
+} 
 ?>
  <!--ask for file name-->
  <h2>Place put this file into that folder where your file is waiting for be extracted work for wordpress website</h2>
