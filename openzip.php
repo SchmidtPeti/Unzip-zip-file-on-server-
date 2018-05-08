b115bc4e48d6c656538cdd9aaa958ee46aebb326
Whichhunter404 <schmidt.peti32@gmail.com>
2018-05-08 19:18:41 +0200

    make a desing for it

--- openzip.php
+++ openzip.php
@@ -8,21 +8,55 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
 </head>
 <div class="container">
-<h2>Wordpress, Drupal and oxwall unzipper</h2>
+<h2>Zipper for extracting your files free</h2>
+<form method="post">
+  <div class="form-group">
+    <label for="zip_folder">Zip folder:</label>
+    <select multiple class="form-control" name="zip_name" id="zip_folder">
+	<?php
+	 $files = glob('*.zip');
+	 for($i=0;$i<count($files);$i++){
+		 echo '<option>'.$files[$i].'</option>';
+	 }
+	?>
+    </select>
+  </div>
+  <div class="form-group">
+    <label for="folder_name">Folder in the zip file:</label>
+	<input id="folder_name" type="text" class="form-control" name="folder_name" placeholder="Folder in the zip or leave empty" value="">
+  </div>
+  <div class="form-group">
+  <input type="submit" name="extract" class="btn btn-success" value="Extract">
+  </div>
 <?php
-highlight_string('A root mappának teljesen üresnek kell lenni a zipelés során');
- $files = glob('*.zip');
- print_r($files);
- $dirname = '';
+if(isset($_POST["extract"])){
+ //$dirname = '';
  $zip = new ZipArchive();
- $res = $zip->open($files[0]);
+ $res = $zip->open($_POST["zip_name"]);
  if($res==true){
 	 $zip->extractTo($_SERVER['DOCUMENT_ROOT']);
 	 $zip->close();
-	 echo "királyság";
+	 echo '<div class="alert alert-success">
+  <strong>Success!</strong> Zip is extracted.
+</div>';
  }
-  $dir = glob($dirname.'*',GLOB_ONLYDIR);
-  print_r($dir);
+ //$dir = glob($dirname.'*',GLOB_ONLYDIR);
+ //print_r($dir);
+ $dst = $_SERVER['DOCUMENT_ROOT'];
+ if($_POST["folder_name"]!=""){
+	echo'<div class="alert alert-info">
+  <strong>info!</strong> Waitinf for copying....
+</div>';
+ recurse_copy($_POST["folder_name"],$dst);
+ echo '<div class="alert alert-success">
+  <strong>Success!</strong> Copy all file from folder is done!.
+</div>';
+ }
+ else{
+	echo '<div class="alert alert-success">
+  <strong>Success!</strong> Copy all file from folder is done!.
+</div>'; 
+ }
  function recurse_copy($src,$dst) { 
     $dir = opendir($src); 
     @mkdir($dst); 
@@ -38,5 +72,7 @@ highlight_string('A root mappának teljesen üresnek kell lenni a zipelés sorá
     } 
     closedir($dir); 
 } 
+}
 ?>
+</form>
 </div>
