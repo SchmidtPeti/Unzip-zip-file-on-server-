fca622debbd0762d08e40ced052e368bb12e34a4
Whichhunter404 <schmidt.peti32@gmail.com>
2020-02-16 13:41:21 +0100

    folder moving is done, but folder moving is not

--- openzip.php
+++ openzip.php
@@ -15,8 +15,8 @@
         //$dirname = '';
         $zip = new ZipArchive();
         $res = $zip->open($_POST["zip_name"]);
-        $dst = $_SERVER['DOCUMENT_ROOT'];
-
+        $dst = './';
+        $was_Extracion = false;
 
         if($res==true){
             $zip->extractTo($dst);
@@ -24,56 +24,57 @@
             echo '<div class="alert alert-success">
             <strong>Success!</strong> Zip is extracted.
             </div>';
+            $was_Extracion = true;
+        }
+        else{
+            echo '<h2>Zip extraction is failed!</h2>';
         }
 
 
-
-        if($_POST["folder_name"]!=""){
+        if($_POST["folder_name"]!=""&&$was_Extracion){
             echo'<div class="alert alert-info">
-                <strong>info!</strong> Waiting for copying....
+                <strong>info!</strong> Waiting for copying all the files here from that specified folder...
             </div>';
             try {
-                recurse_copy($_POST["folder_name"], $dst);
+                rcopy($_POST["folder_name"], $dst);
             }
             catch (Exception $exception){
-                echo '<div class="alert alert-danger"><strong>Error</strong>You gave me folder which do not exist in the zip folder</div>';
+                echo '<div class="alert alert-danger"><strong>Error</strong>You gave me folder which was not in the zip folder</div>';
             }
             echo '<div class="alert alert-success">
-                    <strong>Success!</strong> Move out all of the file from folder!.
+                    <strong>Success!</strong> Move out all of the files here from the folder!.
             </div>';
         }
-        function recurse_copy($src,$dst) {
-            $dir = opendir($src);
-            @mkdir($dst);
-            while(false !== ( $file = readdir($dir)) ) {
-                if (( $file != '.' ) && ( $file != '..' )) {
-                    if ( is_dir($src . '/' . $file) ) {
-                        recurse_copy($src . '/' . $file,$dst . '/' . $file);
-                    }
-                    else {
-                        copy($src . '/' . $file,$dst . '/' . $file);
-                    }
-                }
-            }
-            closedir($dir);
-        }
-    }
+        function rcopy($src, $dst) {
+            if (file_exists ( $dst ))
+                rrmdir ( $dst );
+            if (is_dir ( $src )) {
+                mkdir ( $dst );
+                $files = scandir ( $src );
+                foreach ( $files as $file )
+                    if ($file != "." && $file != "..")
+                        rcopy ( "$src/$file", "$dst/$file" );
+            } else if (file_exists ( $src ))
+                copy ( $src, $dst );
+        }    }
     ?>
-<h2>Extract your zip file from serve</h2>
-<p>Place the "openzip.php" file to the root folder and move your zip file </p>
+<h2>Extract your zip file from server</h2>
+<p>Place the "openzip.php" file to the root folder and move your zip file there</p>
+<p>You can set a folder name(which is in the zip file) and all of its content will be put here</p>
 <form method="post">
   <div class="form-group">
-    <label for="zip_folder">Found zip file in this folder:</label>
+    <label for="zip_folder">Found zip files in this folder:</label>
 
     <select multiple class="form-control" name="zip_name" id="zip_folder">
 	<?php
 	 $files = glob('*.zip');
-	 if(count($files)<1){
-         echo '<option value="" disabled>Place your zip file in the folder where this file is</option>';
-     }
-	 else {
-         for ($i = 0; $i < count($files); $i++) {
-             echo '<option value="'. $files[$i].'">' . $files[$i] . '</option>';
+	 if(!$files!==null) {
+         if (count($files) < 1) {
+             echo '<option value="" disabled>Place your zip file in the folder where this file is</option>';
+         } else {
+             for ($i = 0; $i < count($files); $i++) {
+                 echo '<option value="' . $files[$i] . '">' . $files[$i] . '</option>';
+             }
          }
      }
 	?>
